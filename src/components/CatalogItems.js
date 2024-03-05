36c0b3e01630851c6c7c82d34fe62e8c92d0b794
Jawad Yazbek <jawadyazbek@gmail.com>
2024-03-05 22:10:23 +0200

    added stars

--- src/components/CatalogItems.js
+++ src/components/CatalogItems.js
@@ -10,7 +10,7 @@ import {
 } from "@mui/material";
 import React from "react";
 import Carousel from "react-material-ui-carousel";
-
+import StarIcon from '@mui/icons-material/Star';
 export default function CatalogItems({ data }) {
   var items = [{}, {}, {}, {}];
   function Image() {
@@ -32,11 +32,12 @@ export default function CatalogItems({ data }) {
         <>
           <Grid xs={12} sm={6} md={3} lg={2} >
             <Card
+          
               variant="none"
               style={{ boxShadow: 0, margin: 10 }}
-              sx={{ maxHeight: "373", maxWidth: "100%" }}
+              sx={{borderRadius:12, maxHeight: "373", maxWidth: "100%" }}
             >
-                <CardActionArea >
+                <CardActionArea  >
               <Carousel
                 autoPlay={false}
                 children={items.map((item, i) => (
@@ -45,13 +46,28 @@ export default function CatalogItems({ data }) {
               />
 
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {value.country},{value.city}
+                <Grid container>
+                  <Grid item xs={10}> <Typography sx={{ fontWeight:'bold' }} gutterBottom variant="body1" margin={0} component="div">
+                  {value.country},{value.city} 
+                </Typography></Grid>
+                <Grid item xs={2}   justifyContent={'center'}>
+                 <Typography display={'flex'}> <StarIcon sx={{ width:'25px',height:'25px' }}/> {value.stars}</Typography>
+                </Grid>
+                </Grid>
+               
+                <Typography gutterBottom variant="body1"  margin={0} component="div">
+                {value.rental_name}
                 </Typography>
+               
+                
                 <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
+                {value.date_available} Nights 
                 </Typography>
+                
+                <Typography sx={{ fontWeight:'bold',textDecoration:'underline' }} variant="body2" color="text.secondary">
+                {value.price_per_night}$ total before taxes
+                </Typography>
+
               </CardContent>
               </CardActionArea>
             </Card>
